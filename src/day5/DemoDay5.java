a8e8ebdcb1661caab8122b0696b4a8d8cedfa2ff
jomue <jomue@DESKTOP-1B4OUSJ>
2023-07-29 13:11:46 -0400

    Final push

--- src/day5/DemoDay5.java
+++ src/day5/DemoDay5.java
@@ -1,14 +1,17 @@
 package day5;
 
 public class DemoDay5 {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("HOTTTT....");
 	}
 
-public static void getData(){
- System.out.println("Niceee");
-}
-	
+	public static void getData() {
+		System.out.println("Niceee");
+	}
+
+	public static void getInfo() {
+		System.out.println("age is 25");
+	}
 }
